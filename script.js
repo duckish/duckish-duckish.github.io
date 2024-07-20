7f7e42e243f1ff49135c32f579c4e9482bc833eb
Van Duc Nguyen <vanduc.nguyen.5@gmail.com>
2024-07-20 09:53:50 -0400

    v

--- script.js
+++ script.js
@@ -5059,12 +5059,16 @@ function genMLB() {
             'Seattle Mariners', 'St. Louis Cardinals', 'Tampa Bay Rays',
             'Texas Rangers', 'Toronto Blue Jays', 'Washington Nationals']
   
-    twos = ['Diamondbacks', 'Braves', 'Orioles', 'Red Sox', 'Cubs',
-            'White Sox', 'Reds', 'Guardians', 'Rockies', 'Tigers',
-            'Astros', 'Royals', 'Angels', 'Dodgers', 'Marlins',
-            'Brewers', 'Twins', 'Mets', 'Yankees', 'Athletics',
-            'Phillies', 'Pirates', 'Padres', 'Giants', 'Mariners',
-            'Cardinals', 'Rays', 'Rangers', 'Blue Jays', 'Nationals']
+    twos = ['Arizona Diamondbacks', 'Atlanta Braves', 'Baltimore Orioles',
+    'Boston Red Sox', 'Chicago Cubs', 'Chicago White Sox',
+    'Cincinnati Reds', 'Cleveland Guardians', 'Colorado Rockies',
+    'Detroit Tigers', 'Houston Astros', 'Kansas City Royals',
+    'Los Angeles Angels', 'Los Angeles Dodgers', 'Miami Marlins',
+    'Milwaukee Brewers', 'Minnesota Twins', 'New York Mets',
+    'New York Yankees', 'Oakland Athletics', 'Philadelphia Phillies',
+    'Pittsburgh Pirates', 'San Diego Padres', 'San Francisco Giants',
+    'Seattle Mariners', 'St. Louis Cardinals', 'Tampa Bay Rays',
+    'Texas Rangers', 'Toronto Blue Jays', 'Washington Nationals']
  
   
 
